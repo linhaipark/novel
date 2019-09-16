63e3871ea1b8b646f2778959bb361a94584a77e0
linhai <1632141075@qq.com>
2019-09-16 22:18:35 +0800

    home

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +15,9 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -39,4 +44,4 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
-from app import models, errors
+from app import routes, models, errors
